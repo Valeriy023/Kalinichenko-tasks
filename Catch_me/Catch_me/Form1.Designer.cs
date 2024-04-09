ea66408fc3d3c5b832219ad5724322c8a6ddbda8
Valeriy Gushchin <valero1610@yandex.ru>
2024-04-09 11:23:15 +0300

    Max workspace was created

--- Catch_me/Catch_me/Form1.Designer.cs
+++ Catch_me/Catch_me/Form1.Designer.cs
@@ -57,11 +57,11 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(256, 159);
+            this.label1.Location = new System.Drawing.Point(167, 154);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(272, 31);
+            this.label1.Size = new System.Drawing.Size(459, 31);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Мне больше 5 лет?";
+            this.label1.Text = "Я куплю машину в этом месяце?";
             // 
             // Form1
             // 
